6fd37ad47491ee7efe85b767146a68acd329841e
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2025-09-05 03:35:21 +0600

    Registration form design

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gym Management System</title>
+    <link rel="stylesheet" href="design.css">
+</head>
+
+<body>
+    <!--HOME SECTION DESIGN-->
+    <section class="home" id="home">
+        <h2>Gym Management System</h2>
+        <h4>Level Up Your Fitness — In a Smart Way</h4>
+    </section>
+
+    <!--Member Registration Section-->
+    <section class="mem_reg" id="mem_reg">
+        <div class="mem_form">
+        <h2>Register here!</h2>
+
+        <!-- Show session messages -->
+        <?php
+        if (isset($_SESSION['register_error'])) {
+            echo "<p align='center' style='color: red; font-weight: bold;'>" . $_SESSION['register_error'] . "</p>";
+            unset($_SESSION['register_error']); // clear message after showing
+        }
+        if (isset($_SESSION['register_success'])) {
+            echo "<p align='center' style='color: green; font-weight: bold;'>" . $_SESSION['register_success'] . "</p>";
+            unset($_SESSION['register_success']);
+        }
+
+        ?>
+        <br>
+        <form action="register.php" method="post">
+            <label>NAME: </label>
+            <input type="text" name="Name" placeholder="Enter Your Full Name" required><br><br>
+             <label>AGE: </label>
+            <input type="number" name="Age" placeholder="Enter Your Age" required><br><br>
+            <label>EMAIL: </label>
+            <input type="email" name="Email" placeholder=" Enter Your Email" required><br><br>
+            <label>ADDRESS</label>
+            <input type="adress" name="Adress" placeholder="Enter your Adress" required><br><br>
+            <label>GENDER: </label>
+            <div class="gender">
+            <input type="radio" value="male" name="gender"><label for="male">MALE</label>
+            <input type="radio" value="female" name="gender"><label for="female">FEMALE</label>
+        </div>
+            <br><br>
+            <label>PASSWORD: </label>
+            <input type="password" name="Password" placeholder="Enter Password" required><br><br>
+            <select name="Role" placeholder="Select Your Role" required>
+                <option value="">--Select Role--</option>
+                <option value="User">Member</option>
+            </select><br><br>
+            <button type="submit" name="register">Register</button>
+        </form>
+        <br>
+        <p>Already have Account? <a href="login.php">login </a>here!</p>
+    </div>
+    </section>
+</body>
+
+</html>
