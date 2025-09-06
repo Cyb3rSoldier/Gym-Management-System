4cae8f669ee270a1c10d52e5c71b58175dec03d9
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2025-09-06 15:33:01 +0600

    admin, trainer and member login UI added

--- index.php
+++ index.php
@@ -9,6 +9,20 @@
 </head>
 
 <body>
+
+    <!--NAV BAR SECTION-->
+
+    <nav class="navbar">
+        <!--<a href="#" class="logo"><b><big>OMAR.</big></b></a>-->
+        <ul id="nav-ul">
+            <li class="active"><a href="#home">Home</a></li>
+            <li id="nav-li"><a href="#mem_reg">Member Registration</a></li>
+            <li id="nav-li"><a href="member_login.php">Member Dashboard</a></li>
+            <li id="nav-li"><a href="trainer_login.php">Trainer Dashboard</a></li>
+            <li id="nav-li"><a href="admin_login.php">Admin Dashboard</a></li>
+        </ul>
+    </nav>
+
     <!--HOME SECTION DESIGN-->
     <section class="home" id="home">
         <h2>Gym Management System</h2>
@@ -16,49 +30,49 @@
     </section>
 
     <!--Member Registration Section-->
-    <section class="mem_reg" id="mem_reg">
+    <section class="form" id="mem_reg">
         <div class="mem_form">
-        <h2>Register here!</h2>
+            <h2><u>Register here!</u></h2>
 
-        <!-- Show session messages -->
-        <?php
-        if (isset($_SESSION['register_error'])) {
-            echo "<p align='center' style='color: red; font-weight: bold;'>" . $_SESSION['register_error'] . "</p>";
-            unset($_SESSION['register_error']); // clear message after showing
-        }
-        if (isset($_SESSION['register_success'])) {
-            echo "<p align='center' style='color: green; font-weight: bold;'>" . $_SESSION['register_success'] . "</p>";
-            unset($_SESSION['register_success']);
-        }
+            <!-- Show session messages -->
+            <?php
+            if (isset($_SESSION['register_error'])) {
+                echo "<p align='center' style='color: red; font-weight: bold;'>" . $_SESSION['register_error'] . "</p>";
+                unset($_SESSION['register_error']); // clear message after showing
+            }
+            if (isset($_SESSION['register_success'])) {
+                echo "<p align='center' style='color: green; font-weight: bold;'>" . $_SESSION['register_success'] . "</p>";
+                unset($_SESSION['register_success']);
+            }
 
-        ?>
-        <br>
-        <form action="register.php" method="post">
-            <label>NAME: </label>
-            <input type="text" name="Name" placeholder="Enter Your Full Name" required><br><br>
-             <label>AGE: </label>
-            <input type="number" name="Age" placeholder="Enter Your Age" required><br><br>
-            <label>EMAIL: </label>
-            <input type="email" name="Email" placeholder=" Enter Your Email" required><br><br>
-            <label>ADDRESS</label>
-            <input type="adress" name="Adress" placeholder="Enter your Adress" required><br><br>
-            <label>GENDER: </label>
-            <div class="gender">
-            <input type="radio" value="male" name="gender"><label for="male">MALE</label>
-            <input type="radio" value="female" name="gender"><label for="female">FEMALE</label>
+            ?>
+            <br>
+            <form action="" method="post">
+                <label>NAME: </label>
+                <input type="text" name="Name" placeholder="Enter Your Full Name" required><br><br>
+                <label>AGE: </label>
+                <input type="number" name="Age" placeholder="Enter Your Age" required><br><br>
+                <label>EMAIL: </label>
+                <input type="email" name="Email" placeholder=" Enter Your Email" required><br><br>
+                <label>ADDRESS</label>
+                <input type="adress" name="Adress" placeholder="Enter your Adress" required><br><br>
+                <label>GENDER: </label>
+                <div class="gender">
+                    <input type="radio" value="male" name="gender"><label for="male">MALE</label>
+                    <input type="radio" value="female" name="gender"><label for="female">FEMALE</label>
+                </div>
+                <br><br>
+                <label>PASSWORD: </label>
+                <input type="password" name="Password" placeholder="Enter Password" required><br><br>
+                <select name="Role" placeholder="Select Your Role" required>
+                    <option value="">--Select Role--</option>
+                    <option value="User">Member</option>
+                </select><br><br>
+                <button type="submit" name="register">Register</button>
+            </form>
+            <br>
+            <p>Already have Account? <a href="login.php">login </a>here!</p>
         </div>
-            <br><br>
-            <label>PASSWORD: </label>
-            <input type="password" name="Password" placeholder="Enter Password" required><br><br>
-            <select name="Role" placeholder="Select Your Role" required>
-                <option value="">--Select Role--</option>
-                <option value="User">Member</option>
-            </select><br><br>
-            <button type="submit" name="register">Register</button>
-        </form>
-        <br>
-        <p>Already have Account? <a href="login.php">login </a>here!</p>
-    </div>
     </section>
 </body>
 
---
+++ trainer_login.php
@@ -0,0 +1,34 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trainer Login Page</title>
+    <link rel="stylesheet" href="design.css">
+</head>
+<body>
+    <div class="form">
+        <h2><b><u>Login Here!</u></b></h2><br>
+
+         <!-- Show session messages -->
+        <?php
+        if (isset($_SESSION['login_error'])) {
+            echo "<p align='center' style='color: red; font-weight: bold;'>" . $_SESSION['login_error'] . "</p>";
+            unset($_SESSION['login_error']); // clear message after showing
+        }
+
+        ?>
+        <br>
+        <form action="" method="post">
+            <input type="Email" name="Email" placeholder="Email" required><br><br>
+            <input type="Password" name="Password" placeholder="Password" required><br><br>
+            <button type="submit" name="login">login</button>
+        </form>
+        <br>
+    </div>
+    <br><br>
+    <a class="backHome" href="index.php"><u>Back to HOME</u></a>
+</body>
+</html>
