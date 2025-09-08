2975a9a470bbc5add6cc25eb39ce956cc91e8415
Omar Faruk Rakib <omarfarukrakib26@gmail.com>
2025-09-09 01:47:21 +0600

    database connected

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+//this php fie is for database( gym-management-system ) connection!
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "gym-management-system";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if($conn->connect_error){
+    die("Connection failed: ". $conn->connect_error);
+}
+
+?>
